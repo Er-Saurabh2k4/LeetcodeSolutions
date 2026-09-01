b6e318e84c9350e028020065a59d6f71983da24a
Er-Saurabh2k4 <saurabhydv189@gmail.com>
2026-09-01 21:12:23 +0530

    Time: 1 ms (31.61%) | Memory: 28.2 MB (77.81%) - LeetSync

---
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        //see the pattern and solve using trick 
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i+=2){
+            swap(nums[i],nums[i+1]);
+        }
+        return nums;
+    }
+};
