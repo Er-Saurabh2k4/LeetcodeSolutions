6f8beabf6411f9eb9ace896d3b7a81b1372fc209
Er-Saurabh2k4 <saurabhydv189@gmail.com>
2026-09-12 21:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (82.31%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,18 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> findWordsContaining(vector<string>& words, char x) {
+//         vector<int>ans;
+//         for(int i=0;i<words.size();i++){
+//             string s=words[i];
+//             int j=0;
+//             while(j<s.size()){
+//                 if(s[j]==x){
+//                     ans.push_back(i);
+//                     break;
+//                 }
+//                 j++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int>ans;
-        for(int i=0;i<words.size();i++){
-            string s=words[i];
-            int j=0;
-            while(j<s.size()){
-                if(s[j]==x){
+        vector<int> ans;
+
+        for (int i = 0; i < words.size(); i++) {
+            for (char c : words[i]) {
+                if (c == x) {
                     ans.push_back(i);
                     break;
                 }
-                j++;
             }
         }
+
         return ans;
     }
 };
